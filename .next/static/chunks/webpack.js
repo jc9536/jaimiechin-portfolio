af62fd04b5cadc354d3eb911cca2a4c69917c322
Jaimie Chin <jc9536@nyu.edu>
2022-10-07 00:15:31 -0400

    light mode accent color change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ff9cd67ca96d615"; }
+/******/ 		__webpack_require__.h = function() { return "dbb79ee733d71437"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
