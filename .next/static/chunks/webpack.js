23261a106c6059620e485c35977f8f20e28375af
Jaimie Chin <jc9536@nyu.edu>
2022-10-08 17:58:59 -0400

    removed favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c94320ba91066d3e"; }
+/******/ 		__webpack_require__.h = function() { return "93f190a948dc8ade"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
