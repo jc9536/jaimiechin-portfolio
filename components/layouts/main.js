50aef7f3776088647e13bdd9cf1582d329cb6b46
Jaimie Chin <jc9536@nyu.edu>
2022-10-08 18:21:35 -0400

    favicon attempt 3

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,10 +8,9 @@ const Main = ({ children, router}) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/public/images/favicon.ico?v=1" type="image/x-icon" />
-                <link rel="shortcut icon" href="/public/images/favicon.ico?v=1" type="image/x-icon" />
+                <link rel="icon" href="/favicon.ico?v=1" type="image/x-icon" />
+                <link rel="shortcut icon" href="/favicon.ico?v=1" type="image/x-icon" />
                 <title>Jaimie Chin - Homepage</title>
-                
             </Head>
 
             <Navbar path={router.asPath} />
