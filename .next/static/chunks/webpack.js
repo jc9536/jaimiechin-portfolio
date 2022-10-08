3131e4da2cf1a20d687e08b42fb9ea8ba315f68b
Jaimie Chin <jc9536@nyu.edu>
2022-10-08 17:48:09 -0400

    favicon test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5eeff1f6dcd7b106"; }
+/******/ 		__webpack_require__.h = function() { return "1e70fd351be4acbf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,7 +8,16 @@ const Main = ({ children, router}) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Jaimie Chin's portfolio homepage"/>
+                <mata name="author" content="Jaimie Chin"/> 
+                <link rel="icon" href="/images/favicon.ico?" type="image/x-icon" />
+                <link rel="shortcut icon" href="/images/favicon.ico?" type="image/x-icon" />
+                <meta property="og:site_name" content="Takuya Matsuyama" />
+                <meta name="og:title" content="Jaimie Chin" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/JaimieChin.png" />
                 <title>Jaimie Chin - Homepage</title>
+                
             </Head>
 
             <Navbar path={router.asPath} />
