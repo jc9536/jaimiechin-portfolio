0eb85c3bb57d2df1ebdf9edeef91a63c4bb134c5
Jaimie Chin <jc9536@nyu.edu>
2022-10-09 04:06:05 -0400

    fix uxWorks component

--- pages/uxWorks/Sheepy.js
+++ pages/uxWorks/Sheepy.js
@@ -1,11 +1,8 @@
 import { Container, Badge, useColorModeValue} from '@chakra-ui/react'
-import { Title, WorkImage} from '../../components/uxWorks'
+import { Title, WorkImage} from '../../components/UXworks'
 import Layout from '../../components/layouts/article'
 
 const uxWork = () => {
-    const ProjInfo = `/images/sheepyCaseStudy/ProjectInfo${useColorModeValue('', '_dark')}.png`
-    const Concept = `/images/sheepyCaseStudy/Concept${useColorModeValue('', '_dark')}.png`
-    const MarketNeeds = `/images/sheepyCaseStudy/marketNeeds${useColorModeValue('', '_dark')}.png`
     return (
         <Layout title="Sheepy">
             <br />
@@ -15,9 +12,9 @@ const uxWork = () => {
                 </Title>
          
                 <WorkImage src="/images/sheepyCaseStudy/matrix.png" alt="MatrixCover" />
-                <WorkImage src={ProjInfo} alt="ProjectInformation" />
-                <WorkImage src={Concept} alt="Concept" />
-                <WorkImage src={MarketNeeds} alt="SheepyMarketNeeds" />
+                <WorkImage src={`/images/sheepyCaseStudy/ProjectInfo${useColorModeValue('', '_dark')}.png`} alt="ProjectInformation" />
+                <WorkImage src={`/images/sheepyCaseStudy/Concept${useColorModeValue('', '_dark')}.png`} alt="Concept" />
+                <WorkImage src={`/images/sheepyCaseStudy/marketNeeds${useColorModeValue('', '_dark')}.png`} alt="SheepyMarketNeeds" />
 
             </Container>
         </Layout>
