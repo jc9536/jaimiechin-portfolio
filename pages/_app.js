0348969283d0ef8b58d5567a2b9bfc5bd9c0a21d
Jaimie Chin <jc9536@nyu.edu>
2022-11-21 21:45:02 -0500

    google analyitics attempt

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
 import { Analytics } from '@vercel/analytics/react'
-import { GoogleAnalytics } from 'nextjs-google-analytics'
+import Script from 'next/script'
 
 const Website = ({ Component, pageProps, router }) => {
     return (
@@ -12,7 +12,19 @@ const Website = ({ Component, pageProps, router }) => {
             <Fonts />
             <Layout router={router}>
                 <AnimatePresence exitBeforeEnter initial={true}>
-                    <GoogleAnalytics trackPageViews />
+                    <Script
+                        src="https://www.googletagmanager.com/gtag/js?id=G-xxxxxxxxxx"
+                        strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){window.dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-G-P4NPYDG6ZT');
+                            `}
+                    </Script>
                     <Component {...pageProps} key={router.route} />
                     <Analytics />
                 </AnimatePresence>
