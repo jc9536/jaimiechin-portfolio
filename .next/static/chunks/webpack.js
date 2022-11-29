06053a1521ae785f98566becac99ff9a6d0d47e8
Jaimie Chin <jc9536@nyu.edu>
2022-11-29 01:42:24 -0500

    fix typos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c1e5ccddd258d2a4"; }
+/******/ 		__webpack_require__.h = function() { return "0039d28f0b661f22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
