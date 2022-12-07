f5704ee43270b1746332c083ef4c5b0355b907b8
Jaimie Chin <jc9536@nyu.edu>
2022-12-06 22:14:19 -0500

    fix blender figure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b78f68b2a5335c7"; }
+/******/ 		__webpack_require__.h = function() { return "e5b89944a2533a94"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
