72c2b2f16d175c2092a3b59ba5e90c47582209c5
Jaimie Chin <jc9536@nyu.edu>
2023-01-05 19:10:11 -0500

    update Fairness Case Study implementation and validation panels

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dfef96df6c49791d"; }
+/******/ 		__webpack_require__.h = function() { return "0c2175701c234736"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a2c0ac56894d99bd.hot-update.js
+++ .next/static/webpack/webpack.a2c0ac56894d99bd.hot-update.js
@@ -63,7 +63,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "018f8e7ed7c860d0"; }
+/******/ 	__webpack_require__.h = function() { return "0c2175701c234736"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/jsonp chunk loading */
