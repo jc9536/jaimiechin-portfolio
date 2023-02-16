fba02e23923c32c7a6aa6de0aef48a7c7a60cfb6
Jaimie Chin <jc9536@nyu.edu>
2023-02-16 17:52:57 -0500

    fix typo in fairness

--- .next/static/webpack/webpack.01a8bf304e88c593.hot-update.js
+++ .next/static/webpack/webpack.01a8bf304e88c593.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7899e0100c96a57b"; }
+/******/ 	__webpack_require__.h = function() { return "1713e41d49261125"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
