05f48cceb5a8c58b53934a771f370914cfb19479
Jaimie Chin <jc9536@nyu.edu>
2024-03-11 21:51:10 -0700

    update email link and resume

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 <Stack justify="space-between" direction="row" align="center">
                     <ButtonGroup variant="ghost">
                         <IconButton as="a" target='_blank' href="https://github.com/jc9536?tab=repositories" aria-label="GitHub" colorScheme='yellow' icon={<FaGithub fontSize="1.75rem" />} />
-                        <IconButton as="a" href="mailto: jc9536@nyu.edu" aria-label="Gmail" colorScheme='yellow' icon={<FaEnvelope fontSize="1.75rem" />} />
+                        <IconButton as="a" href="mailto: chinjaimie@gmail.com" aria-label="Gmail" colorScheme='yellow' icon={<FaEnvelope fontSize="1.75rem" />} />
                         <IconButton as="a" target='_blank' href="https://www.linkedin.com/in/jaimie-chin-a38511236/" aria-label="Linkedin" colorScheme='yellow' icon={<FaLinkedin fontSize="1.75rem" />} />
                     </ButtonGroup>
                 </Stack>
