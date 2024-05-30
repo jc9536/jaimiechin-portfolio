b111fcc4c7345f32a503289be78b7ff149baf0c5
Jaimie Chin <jc9536@nyu.edu>
2024-05-29 20:25:48 -0700

    update final project image error

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"digitalFabrication/A8FinalProject.js -> image-map-resizer":{"id":"digitalFabrication/A8FinalProject.js -> image-map-resizer","files":["static/chunks/node_modules_image-map-resizer_js_imageMapResizer_min_js.js"]}}
